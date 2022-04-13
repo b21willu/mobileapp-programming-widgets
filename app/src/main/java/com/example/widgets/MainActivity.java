76e25196f3255a86a1397bf4198289aba1d3c9e1
b21willu <Wilmer990302>
2022-04-13 13:08:17 +0200

    Removed some code that were added but not needed for the assignment. Code for functions onPostResume, onStart, onStop and onDestroy were removed.

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -8,30 +8,6 @@ import android.util.Log;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onPostResume() {
-        super.onPostResume();
-        Log.d("==>", "MainActivity resumed");
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d("==>", "MainActivity started");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d("==>", "MainActivity stopped");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d("==>", "MainActivity destroyed");
-    }
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
